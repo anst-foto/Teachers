c61a81c962343369f52562e9d1f777eeb55c9f25
Starinin Andrey <starinin-andrey@ya.ru>
2025-09-16 20:02:56 +0300

    Создание класса для сохранения или получения данных

---
+++ Teachers.Desktop/DataAccesses/JsonService.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text.Json;
+using Teachers.Desktop.Models;
+
+namespace Teachers.Desktop.DataAccesses;
+
+public static class JsonService
+{
+    public static void Save(IEnumerable<Teacher> teachers, string path = "teachers.json")
+    {
+        var json = JsonSerializer.Serialize(teachers);
+        File.WriteAllText(path, json);
+    }
+
+    public static IEnumerable<Teacher>? Load(string path = "teachers.json")
+    {
+        var json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<IEnumerable<Teacher>>(json);
+    }
+}
