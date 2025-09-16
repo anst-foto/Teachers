aefbc990412ad7e3d220e47a6ae3cc2fc0b667ed
Starinin Andrey <starinin-andrey@ya.ru>
2025-09-16 21:09:09 +0300

    Написание ручных тестов

--- Teachers.ConsoleTest/Program.cs
+++ Teachers.ConsoleTest/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Teachers.DataAccess;
+using Teachers.Models;
 
-Console.WriteLine("Hello, World!");
+var teacher = new Teacher()
+{
+    LastName = "Старинин",
+    FirstName = "Андрей",
+    Patronymic = "Николаевич",
+    BirthDate = new DateTime(1980, 1, 1),
+    Phone = "+79042144491",
+    Faculty = "SoftDev"
+};
+teacher.Subjects.Add("C#");
+teacher.Subjects.Add("SQL");
+
+var teachers = new List<Teacher> { teacher };
+
+JsonService.Save(teachers);
+
+var loadedTeachers = JsonService.Load();
+foreach (var t in loadedTeachers)
+{
+    Console.WriteLine(t == teacher);
+    Console.WriteLine(t);
+}
