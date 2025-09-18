e2b32f869d55a626801041c3471b91f13d090326
Starinin Andrey <starinin-andrey@ya.ru>
2025-09-18 20:07:39 +0300

    Добавление исключений

---
+++ Teachers.DataAccess/Exceptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teachers.DataAccess;
+
+public class SaveToJsonException(string path, Exception innerException)
+    : Exception($"Ошибка сохранения JSON в {path}", innerException);
+
+public class LoadFromJsonException : Exception
+{
+    public LoadFromJsonException(string path, Exception? innerException = null)
+        : base($"Ошибка загрузки JSON из {path}", innerException) { }
+}
--- Teachers.DataAccess/JsonService.cs
+++ Teachers.DataAccess/JsonService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Encodings.Web;
 using System.Text.Json;
@@ -10,19 +11,37 @@ public static class JsonService
 {
     public static void Save(IEnumerable<Teacher> teachers, string path = "teachers.json")
     {
-        var options = new JsonSerializerOptions
+        try
         {
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            WriteIndented = true
-        };
-        options.Converters.Add(new CustomDateTimeConverter("yyyy-MM-dd"));
-        var json = JsonSerializer.Serialize(teachers, options);
-        File.WriteAllText(path, json);
+            var options = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true
+            };
+            options.Converters.Add(new CustomDateTimeConverter("yyyy-MM-dd"));
+            var json = JsonSerializer.Serialize(teachers, options);
+            File.WriteAllText(path, json);
+        }
+        catch (Exception e)
+        {
+            throw new SaveToJsonException(path, e);
+        }
     }
 
-    public static IEnumerable<Teacher>? Load(string path = "teachers.json")
+    public static IEnumerable<Teacher> Load(string path = "teachers.json")
     {
-        var json = File.ReadAllText(path);
-        return JsonSerializer.Deserialize<IEnumerable<Teacher>>(json);
+        try
+        {
+            var json = File.ReadAllText(path);
+            var teachers =  JsonSerializer.Deserialize<IEnumerable<Teacher>>(json);
+
+            if (teachers is null) throw new LoadFromJsonException(path);
+
+            return teachers;
+        }
+        catch (Exception e)
+        {
+            throw new LoadFromJsonException(path, e);
+        }
     }
 }
